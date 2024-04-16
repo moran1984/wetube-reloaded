b9cd225dd3454a0c9a33bae7a553cdc12bef3c1e
cloDp <ryan.yun1126@gmail.com>
2024-04-16 12:16:37 +0800

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ const PORT = 4000;
 
 const app = express(); // 시작
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+const handleHome = (req, res) => {
+  return res.send("hi i still love you");
+};
 
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 const handleListening = () =>
   console.log(`❤️  Server is listening on port ${PORT} 😘`);
 
