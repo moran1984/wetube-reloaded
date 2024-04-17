110af44ac93d2ffafcdb088bee2247fc9e8a38cf
cloDp <ryan.yun1126@gmail.com>
2024-04-17 12:33:54 +0800

    #4.1 Making Our Routers

--- src/server.js
+++ src/server.js
@@ -5,17 +5,29 @@ const PORT = 4000;
 
 const app = express(); // 시작
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`❤️  Server is listening on port ${PORT} 😘`);
