ba58686f2936dbcd61d6bd6a3fa5c22ed4d641d8
cloDp <ryan.yun1126@gmail.com>
2024-04-17 09:58:12 +0800

    #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const PORT = 4000;
 
 const app = express(); // 시작
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
@@ -13,12 +13,8 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here" });
-};
+app.get("/", logger, handleHome);
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
 const handleListening = () =>
   console.log(`❤️  Server is listening on port ${PORT} 😘`);
 
