7aa54b2398de4c8ebb8e2aa55b37c4661e135f74
cloDp <ryan.yun1126@gmail.com>
2024-04-19 10:44:43 +0800

    #5.1 Configuring Pug

--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
+console.log(process.cwd());
+
 const app = express(); // 시작
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
