0a4f9f462baead8db1dd725abdc08e5d4f09f720
cloDp <ryan.yun1126@gmail.com>
2024-04-22 10:58:16 +0800

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,14 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => {
-  return res.render("watch");
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
 };
-export const edit = (req, res) => {
-  return res.render("edit");
-};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   return res.send("Delete Video");
 };
