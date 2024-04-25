b0a5703bec32ee4430beca641df12c912abe23fb
cloDp <ryan.yun1126@gmail.com>
2024-04-25 11:08:15 +0800

    #6.11 Our First Query

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`❤️  Server is listening on port ${PORT} 😘`);
+
+app.listen(PORT, handleListening);
