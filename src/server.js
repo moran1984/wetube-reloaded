7e821bfd4d2efe59efb4454a506d27b2a9b93df5
cloDp <ryan.yun1126@gmail.com>
2024-05-13 12:58:58 +0800

    #7.3 Form Validation

--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", `${process.cwd()}/src/views`);
+
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
