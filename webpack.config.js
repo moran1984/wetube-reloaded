8acbb80225d093a17d5694c02d68800b6bcd1a0f
cloDp <ryan.yun1126@gmail.com>
2024-05-28 11:13:44 +0800

    #9.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+const path = require("path");
+
 module.exports = {
+  mode: "development",
   entry: "./src/client/js/main.js",
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
