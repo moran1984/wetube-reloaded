dac5d8bdbf768293c9d733d486ce3db965c45dc9
cloDp <ryan.yun1126@gmail.com>
2024-05-30 17:36:40 +0800

    #11.5 Time Formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -44,12 +44,14 @@ const handleVolumeChange = (event) => {
     muteBtn.innerText = "Unmute";
   }
 };
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substring(11, 19);
 
 const handelLoadedMetadata = () => {
-  totalTime.innerText = Math.floor(video.duration);
+  totalTime.innerText = formatTime(Math.floor(video.duration));
 };
 const handleTimeUpdate = () => {
-  currentTime.innerText = Math.floor(video.currentTime);
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
 };
 
 playBtn.addEventListener("click", handlePlayClick);
